3a9d7bda371929ba95cd901a46a0bc875686ab68
aaron <aaron@localhost.localdomain>
2021-07-12 11:07:44 -0400

    add padding to header logo

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -3,7 +3,7 @@
 	<div
 		class="max-w-4xl gap-x-4 grid grid-rows-header justify-between grid-cols-5 mx-auto items-center h-full"
 	>
-		<img src="/logo_new.svg" width="570" height="570" class="md:row-span-2 md:col-span-1 w-32 h-auto " alt="logo" />
+		<img src="/logo_new.svg" width="570" height="570" class="md:row-span-2 md:col-span-1 w-32 h-auto md:p-2" alt="logo" />
 		<h1 class="uppercase font-Montserrat text-4xl sm:text-5xl  text-gray-900 col-span-4">	<a href="/" class="hover:text-red-600 hover:bold">Asheville Aikikai</a></h1>
 		<nav class="flex justify-between col-span-5 md:col-span-4 self-end">
 			<a sveltekit:prefetch href="/" class="text-sm uppercase text-gray-900 hover:text-red-600 hover:bold">Home</a>
