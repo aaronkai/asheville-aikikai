94ff128f0aebab4bb40f8552e60b9efcd6756ecc
aaron <aaron@localhost.localdomain>
2021-07-14 14:38:03 -0400

    working on integrating site with Sanity CMS

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,5 +1,46 @@
-<footer class="border-t-4 border-gray-900 py-4 flex flex-col items-center justify-center ">
-	<a href="https://aaronhubbard.dev" class="hover:text-brutalred text-gray-900"
+<script>
+	export let dojo="default"
+	let [{dojoName, streetAddress, city, state, zipCode, phoneNumber}] = dojo
+</script>
+
+<footer class="border-t-4 border-gray-900 py-4 flex flex-row justify-around items-center justify-center ">
+
+	<address>
+		<a
+			href="https://goo.gl/maps/sEF3NVsFp7A2"
+			class="text-gray-900  hover:text-red-600"
+		>
+			{dojoName}  {streetAddress} {city}, {state} {zipCode}
+		</a>
+	</address>
+	<a href="tel:{phoneNumber}" class="text-gray-900 hover:text-red-600 "
+	>{phoneNumber}</a
+>
+	<a href="https://aaronhubbard.dev" class="text-gray-900 hover:text-red-600>Site Design">Site Design</a>
+</footer>
+
+<!-- <footer class="border-t-4 border-gray-900 py-4 flex flex-col items-center justify-center ">
+	<section class="sm:col-start-2 sm:col-end-3 mt-4">
+		<h1 class="text-3xl font-Roboto text-gray-900">See You On the Mat!</h1>
+		<h2 class="text-2xl font-Roboto mt-2 text-gray-900">Our Location</h2>
+		<address>
+			<a
+				href="https://goo.gl/maps/sEF3NVsFp7A2"
+				class="text-gray-900 underline hover:text-red-600"
+			>
+				50 Glendale Ave. <br />Asheville, NC 28803
+			</a>
+		</address>
+
+		<h2 class="text-2xl font-Roboto mt-2 text-gray-900">Phone</h2>
+		<a href="tel:828-777-2416 text-gray-900" class="text-gray-900 hover:text-red-600 underline"
+			>(828) 777-2416</a
+		>
+	</section>
+
+
+	<section>
+		<a href="https://aaronhubbard.dev" class="hover:text-brutalred text-gray-900"
 		>Aaron Hubbard Web Development</a
 	>
 	<details>
@@ -28,4 +69,7 @@
 			>
 		</p>
 	</details>
-</footer>
+
+	</section>
+
+</footer> -->
--- src/routes/pages/practice.svelte
+++ src/routes/pages/practice.svelte
@@ -1,21 +1,51 @@
 <script context="module">
+	import client from '$lib/client'
 	export const prerender = true;
+
+	export async function load() {
+		const pageQuery = `*[_type == "practicePage"]`
+		const scheduleQuery = `*[_type == "schedule"]`
+		const page = await client.fetch(pageQuery)
+		const schedule = await client.fetch(scheduleQuery)
+		const [{banner, heading, pageText, subheading}] = page
+		return {
+			props: {banner,heading,pageText,subheading,schedule}
+		}
+	}
 </script>
 
-<main class="grid grid-row justify-items-center">
-	<article class=" mx-4 max-w-sm ">
-		<h1 class="font-Roboto text-5xl text-gray-900 mb-8">Adult Practice</h1>
-		<section>
-			<h2 class="font-Roboto text-2xl text-gray-900 mb-4">All Are Welcome!</h2>
+<script>
+	import { urlFor } from '$lib/image-url'
+	export let banner;
+	export let heading = "Adult Practice";
+	export let subheading = "All Are Welcome!";
+	export let pageText = "placeholder";
+	export let schedule = [];
 
-			<p class="pb-2">
-				Asheville Aikikai invites men and women (ages 14 and up) for advanced and beginning
-				practice. Beginners are welcome anytime.
-			</p>
-			<p>
-				If you are interested in observing or joining a class, please feel free to drop in 10
-				minutes prior to any class. Monthly dues are $60, with discounts for families and students.
-			</p>
+	let activeSchedule;
+	schedule.forEach(schedule => {
+		if (schedule.active){
+			activeSchedule = schedule
+		}
+	});
+	console.log(activeSchedule)
+</script>
+
+
+<main class="grid grid-row justify-items-center">
+	<img src={urlFor(banner.asset).width(600).height(400)} alt="martial arts practice" />
+	<article class=" mx-4 max-w-sm ">
+		<h1 class="font-Roboto text-5xl text-gray-900 mb-8">{heading}</h1>
+		<section>
+			<h2 class="font-Roboto text-2xl text-gray-900 mb-4">{subheading}</h2>
+			{#each pageText as element}
+				{#if element.style == "normal"}
+					{#each element.children as child}
+						<p class="mt-4 text-gray-900">{child.text}</p>
+					{/each}
+				{/if}
+			{/each}
+
 		</section>
 		<section>
 			<h2 class="font-Roboto text-2xl text-gray-900 pb-2 mt-8 mb-4">Schedule</h2>
@@ -25,38 +55,38 @@
 				<th class="border-r border-b border-gray-900">Instructor</th>
 				<tr class="border-b border-gray-900 ">
 					<td class="border-r border-gray-900">Monday</td>
-					<td class="border-r border-gray-900">6:30 - 7:30 PM</td>
-					<td>Michael</td>
+					<td class="border-r border-gray-900">{activeSchedule.Monday.startTime} - {activeSchedule.Monday.endTime} {activeSchedule.Monday.amOrPm}</td>
+					<td>{activeSchedule.Monday.instructor}</td>
 				</tr>
 				<tr class="border-b border-gray-900 ">
 					<td class="border-r border-gray-900">Tuesday</td>
-					<td class="border-r border-gray-900">6:30 - 7:30 PM</td>
-					<td>Dan</td>
+					<td class="border-r border-gray-900">{activeSchedule.Tuesday.startTime} - {activeSchedule.Tuesday.endTime} {activeSchedule.Tuesday.amOrPm}</td>
+					<td>{activeSchedule.Tuesday.instructor}</td>
 				</tr>
 				<tr class="border-b border-gray-900 ">
 					<td class="border-r border-gray-900">Wednesday</td>
-					<td class="border-r border-gray-900">6:30 - 7:30 PM</td>
-					<td>Claire</td>
+					<td class="border-r border-gray-900">{activeSchedule.Wednesday.startTime} - {activeSchedule.Wednesday.endTime} {activeSchedule.Wednesday.amOrPm}</td>
+					<td>{activeSchedule.Wednesday.instructor}</td>
 				</tr>
 				<tr class="border-b border-gray-900 ">
 					<td class="border-r border-gray-900">Thursday</td>
-					<td class="border-r border-gray-900">6:30 - 7:30 PM</td>
-					<td>Dan</td>
+					<td class="border-r border-gray-900">{activeSchedule.Thursday.startTime} - {activeSchedule.Thursday.endTime} {activeSchedule.Thursday.amOrPm}</td>
+					<td>{activeSchedule.Thursday.instructor}</td>
 				</tr>
 				<tr class="border-b border-gray-900 ">
 					<td class="border-r border-gray-900">Friday</td>
-					<td class="border-r border-gray-900">6:30 - 7:30 PM</td>
-					<td>Dan</td>
+					<td class="border-r border-gray-900">{activeSchedule.Friday.startTime} - {activeSchedule.Friday.endTime} {activeSchedule.Friday.amOrPm}</td>
+					<td>{activeSchedule.Friday.instructor}</td>
 				</tr>
 				<tr class="border-b border-gray-900 ">
 					<td class="border-r border-gray-900">Saturday</td>
-					<td class="border-r border-gray-900">10:00 – 11:00 AM</td>
-					<td>Dan</td>
+					<td class="border-r border-gray-900">{activeSchedule.Saturday.startTime} - {activeSchedule.Saturday.endTime} {activeSchedule.Saturday.amOrPm}</td>
+					<td>{activeSchedule.Saturday.instructor}</td>
 				</tr>
 				<tr class="border-b border-gray-900 ">
 					<td class="border-r border-gray-900">Sunday</td>
-					<td class="border-r border-gray-900">Closed</td>
-					<td>n/a</td>
+					<td class="border-r border-gray-900">n/a</td>
+					<td>{activeSchedule.Sunday.instructor}</td>
 				</tr>
 			</table>
 		</section>
