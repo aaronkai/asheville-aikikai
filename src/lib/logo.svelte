4386eedf4edb0f6bae5eeffab4fcadbd9ce8ece2
aaron <aaron@localhost.localdomain>
2021-07-22 15:51:30 -0400

    update logo layout and netlify.toml

--- src/lib/logo.svelte
+++ src/lib/logo.svelte
@@ -10,11 +10,10 @@
 </script>
 
 {#if visible}
-<div class="grid gap-4 justify-items-center ">
-	<div class="w-3/4 md:w-1/2 xl:w-1/4 self-end">
+	<div class="w-full h-full ">
 		<svg
-			width="100px"
-			height="100px"
+			width="150px"
+			height="150px"
 			preserveAspectRatio="none" 
 
 			class="w-full h-auto"
@@ -26,7 +25,7 @@
 			<defs id="defs15895" />
 			<g>
 				<path
-					in:fade={{ duration: 2000, delay: 800, easing: quintOut }}
+					in:fade={{ duration: 1000, delay: 500, easing: quintOut }}
 					id="path9676"
 					style="fill:#ff0000;fill-opacity:1;stroke:#ff0000;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
 					d={outer}
@@ -34,28 +33,28 @@
 				/>
 
 				<path
-					in:draw={{ duration: 1000 }}
+					in:draw={{ duration: 500 }}
 					id="path658-6-2"
 					style="fill:none;fill-opacity:1;stroke:#ff0000;stroke-width:2.3;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
 					d={window1}
 					transform="scale(0.26458333)"
 				/>
 				<path
-					in:draw={{ duration: 1000 }}
+					in:draw={{ duration: 500 }}
 					id="path21900"
 					style="fill:none;fill-opacity:1;stroke:#ff0000;stroke-width:2.3;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
 					d={window2}
 					transform="scale(0.26458333)"
 				/>
 				<path
-					in:draw={{ duration: 1000 }}
+					in:draw={{ duration: 500 }}
 					id="path21898"
 					style="fill:none;fill-opacity:1;stroke:#ff0000;stroke-width:2.3;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
 					d={gateOuter}
 					transform="scale(0.26458333)"
 				/>
 				<path
-					in:draw={{ duration: 1000 }}
+					in:draw={{ duration: 500 }}
 					id="path21894"
 					style="fill:none;fill-opacity:1;stroke:#ff0000;stroke-width:2.3;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
 					d={gateInner}
@@ -84,5 +83,4 @@
 			</div>
 		</div> -->
 
-</div>
 {/if}
